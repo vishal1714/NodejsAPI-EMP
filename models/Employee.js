d3645d850043ee7cd2984c52113fd45bd531f7bf
Vishal Ghadage <ghadagevishal993@gmail.com>
2020-08-18 11:32:01 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -32,7 +32,7 @@ const EmployeeSchema = new mongoose.Schema({
   },
   CreatedAt: {
     type: String,
-    default: function() {return moment().format("MMMM Do YYYY, hh:mm:ss a")},
+    default: function() {return moment().format("MMMM Do YYYY, hh:mm:ss A")},
   },
   __v: { type: Number, versionKey: false },
 });
