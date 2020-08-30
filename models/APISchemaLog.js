4eaf97437661d92dce6664e751cb40784a31257b
Vishal Ghadage <ghadagevishal993@gmail.com>
2020-08-31 04:27:40 +0530

    Update APISchemaLog.js

--- models/APISchemaLog.js
+++ models/APISchemaLog.js
@@ -75,6 +75,12 @@ const APISchemaLog = new mongoose.Schema({
   },
 });
 
-//EmployeeSchema.plugin(autoIncrement.plugin, 'Employee');
+APISchemaLog.set('toJSON', {
+     transform: function (doc, ret, options) {
+         ret.id = ret._id;
+         delete ret._id;
+         delete ret.__v;
+     }
+}); 
 
 module.exports = mongoose.model('APILog', APISchemaLog);
