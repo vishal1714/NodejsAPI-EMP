0d1b549d1d372d4822670789e18ce6171494beb7
Vishal Ghadage <ghadagevishal993@gmail.com>
2020-08-31 04:32:35 +0530

    Update APISchemaLog.js

--- models/APISchemaLog.js
+++ models/APISchemaLog.js
@@ -73,7 +73,7 @@ const APISchemaLog = new mongoose.Schema({
 
 APISchemaLog.set('toJSON', {
      transform: function (doc, ret, options) {
-         ret.id = ret._id;
+         ret.RajeRefNo = ret._id;
          delete ret._id;
          delete ret.__v;
      }
