0d36acd65f9de2d9174a85d0523e7cc74778cfcb
Vishal Ghadage <ghadagevishal993@gmail.com>
2020-08-31 22:49:40 +0530

    Update EmployeeAPILog.js

--- controllers/EmployeeAPILog.js
+++ controllers/EmployeeAPILog.js
@@ -5,17 +5,32 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const EmployeeAPILog = require('../models/APISchemaLog');
 
-const Log = async (data) => {
+const Log = async (req, Response, IP, reqKey, reqmethod) => {
   try {
     var m = moment().tz('Asia/Kolkata').format('MMMM Do YYYY, hh:mm:ss A');
-    var LogedinDB = JSON.stringify(data);
+    if (!req.params.id) {
+      var reqbody = req.body;
+    } else {
+      var reqpara = { _id: req.params.id };
+    }
+
+    const ReqRes = {
+      Method: reqmethod,
+      APIKey: reqKey,
+      reqBody: reqbody,
+      reqPath: reqpara,
+      resBody: Response,
+      ClientIP: IP,
+      LoggedAt: m,
+    };
+    var LogedinDB = JSON.stringify(ReqRes);
     var LogData = '|' + m + '|' + LogedinDB;
 
     fs.appendFile(process.env.LOGDIR, LogData, function (err) {
       if (err) throw err;
-      console.log('Loged!' + LogData);
+      //console.log('Loged!' + LogData);
     });
-    await EmployeeAPILog.create(data);
+    await EmployeeAPILog.create(ReqRes);
   } catch (error) {
     console.log(error);
   }
