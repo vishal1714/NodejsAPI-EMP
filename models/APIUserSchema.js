c7c09d0b9e302aa26a0f3a5f69aaa3b7332e8b1f
Vishal1714 <ghadagevishal993@gmail.com>
2020-12-21 23:07:14 +0530

    Key Limit 40

--- models/APIUserSchema.js
+++ models/APIUserSchema.js
@@ -33,9 +33,9 @@ const UserSchema = new mongoose.Schema({
     type: String,
     index: { unique: true },
     minlength: 10,
-    maxlength: 32,
+    maxlength: 40,
     default: function () { return RandomString.generate({
-      length: 32,
+      length: 36,
     }); },
   },
   AESKey: {
