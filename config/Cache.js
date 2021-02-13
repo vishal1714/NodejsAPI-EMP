64b27b587b2c2804833464344ecfe2b66fb312a0
Vishal1714 <ghadagevishal993@gmail.com>
2021-02-13 22:00:17 +0530

    Redis Update

--- config/Cache.js
+++ config/Cache.js
@@ -1,8 +1,10 @@
 const redis = require('redis');
 const { promisify } = require('util');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
 
 const client = redis.createClient({
-  host : 'raje.tech',
+  host : process.env.REDIS_CACHE_URL,
   port : 6379
 });
 const getCacheAsync = promisify(client.get).bind(client);
